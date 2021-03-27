8113e6ebc8a18d0cb3ccfd253712820843f7aa7e
Shivam Malpani <ssmalpani@g.hmc.edu>
2021-03-27 15:11:46 -0700

    lab3 overright

--- Lab4/gym-e206-students-lab-04/traj_planner_ExpPlanner.py
+++ Lab4/gym-e206-students-lab-04/traj_planner_ExpPlanner.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from traj_planner_utils import *
 import numpy as np
 import time
+import copy
 
 class Node():
 
@@ -27,6 +28,24 @@ class Node():
   def euclidean_distance_to_state(self, state):
     return math.sqrt( (self.state[1] - state[1])**2 + (self.state[2] - state[2])**2 )
 
+  def print(self):
+    """ Debugging method: prints all aspects of a node, handling None types properly
+    """
+    if(self != None):
+      state = self.state
+      edge_distance = self.edge_distance
+    else:
+      state = "None"
+      edge_distance = "None"
+    
+    if(self.parent_node != None):
+      parent = self.parent_node.state
+    else:
+      parent = "None"
+    
+    print("State:", state, " Parent:", parent, " Edge Distance:", edge_distance)
+
+
 class Expansive_Planner():
   
   DISTANCE_DELTA = 1.5 #m
@@ -36,15 +55,19 @@ class Expansive_Planner():
   LARGE_NUMBER = 9999999
   
   MAX_NUM_ITERATIONS = 1000
-  MIN_RAND_DISTANCE = 1.0 #m
-  MAX_RAND_DISTANCE = 5.0 #m
+  MIN_RAND_DISTANCE = 1 #m
+  MAX_RAND_DISTANCE = 5 #m
   MEAN_EDGE_VELOCITY = 0.75 #m
-  PLAN_TIME_BUDGET = 2.0 #s
     
-  def __init__(self):
-    self.fringe = []
+  def __init__(self, plan_time_budget=0.5, tree_size_limit=4000, sample_attempt_limit=10000):
+    self.rng = np.random.default_rng() #to generate a random number rfloat = self.rng.random()  
+    self.PLAN_TIME_BUDGET = plan_time_budget #s
+    self.TREE_SIZE_LIMIT = tree_size_limit # To stop the tree from expanding forever if no valid paths exist
+    self.SAMPLE_ATTEMPT_LIMIT = sample_attempt_limit # To stop the code from hanging if it can't sample a valid node
+    
+    
 
-  def construct_traj(self, initial_state, desired_state, objects, walls):
+  def construct_traj(self, initial_state, desired_state, objects, walls, end_time=None):
     """ Construct a trajectory in the X-Y space and in the time-X,Y,Theta space.
         Arguments:
           traj_point_0 (list of floats): The trajectory's first trajectory point with time, X, Y, Theta (s, m, m, rad).
@@ -57,10 +80,33 @@ class Expansive_Planner():
     self.desired_state = desired_state
     self.objects = objects
     self.walls = walls
-    
-    # Add code here to make a traj #
-      
-    return [], self.LARGE_NUMBER
+
+    current_node = Node(initial_state, None, 0)
+    self.add_to_tree(current_node)
+    goal = self.generate_goal_node(current_node, self.desired_state)
+
+    #Continue looking for a node in the end-game region, ensure tree is large enough
+    while(goal is None and len(self.tree) < self.TREE_SIZE_LIMIT): 
+      if(time.perf_counter() > end_time): 
+        # print("NO PATH FOUND - TIME Attempt Limit Reached")
+        return [], self.LARGE_NUMBER
+      current_node = self.generate_random_node(self.sample_random_node())
+      i = 0 
+      #Generate a new random node 
+      while(current_node is None): 
+        if(time.perf_counter() > end_time):
+          # print("NO PATH FOUND - Sample Attempt Limit Reached")
+          return [], self.LARGE_NUMBER
+        current_node = self.generate_random_node(self.sample_random_node())
+        i += 1
+      self.add_to_tree(current_node)
+      goal = self.generate_goal_node(current_node, self.desired_state)
+
+    if(goal is None):  
+      # print("NO PATH FOUND - No Collision Free Paths Found")
+      return [], self.LARGE_NUMBER
+
+    return self.build_traj(goal)
     
   def construct_optimized_traj(self, initial_state, desired_state, objects, walls):
     """ Construct the best trajectory possible within a limited time budget.
@@ -71,33 +117,58 @@ class Expansive_Planner():
           best_traj (list of lists): A list of trajectory points with time, X, Y, Theta (s, m, m, rad).
           best_traj_cost (float): The path lenght of the shortest traj (m).
     """
-    start_time = time.perf_counter()
     best_traj = []
     best_traj_cost = self.LARGE_NUMBER
-    
-    # Add code here to make many trajs within a time budget and return the best traj #
-    # You will want to call construct_traj #
+    total_trials = 0
+    successful_trials = 0
+
+    error_margin = 0.01 #sec
+    time_budget_exceeded = False
+    start_time = time.perf_counter()
+    end_time = start_time + self.PLAN_TIME_BUDGET
+
+    while (time.perf_counter() < end_time):
+      total_trials +=1 
+      end_time_per_trial = min(end_time, time.perf_counter() + (self.PLAN_TIME_BUDGET/3))
+      traj, traj_dist = self.construct_traj(initial_state, desired_state, objects, walls, end_time)
+      if(traj_dist < self.LARGE_NUMBER):
+        successful_trials += 1
+      if(traj_dist < best_traj_cost): 
+        best_traj = traj 
+        best_traj_cost = traj_dist 
+
+    elapsed_time = time.perf_counter()- start_time
+    # print("---Optimized Trajectory---")
+    # print("Time Elapsed", elapsed_time)
+    if(elapsed_time-self.PLAN_TIME_BUDGET > error_margin):
+      # print("TIME BUDGET EXCEEDED!")
+      time_budget_exceeded = True
+    # print("Best Traj Cost: ", best_traj_cost)
+    # print("Total Paths Attempted: ", total_trials)
+    # print("Successful Paths: ", successful_trials)
+    # print()
       
-    return best_traj, best_traj_cost
+    return best_traj, best_traj_cost, total_trials, successful_trials, time_budget_exceeded
     
   def add_to_tree(self, node):
     """ Add the node to the tree.
         Arguments:
           node (Node): The node to be added.
     """
-    
-    # Add code here to add a node to the tree#
-    pass
+    # print(node.state)
+    # if(node.parent_node is not None):
+      # print("parent:", node.parent_node.state)
+      # print()
+    self.tree.append(node)
+    return 
     
   def sample_random_node(self):
     """ Randomly select a node from the tree and return it.
         Returns:
           node (Node): A randomly selected node from the tree.
     """
-        
-    # Add code here to return a random node from the tree #
-    
-    return None
+    #Naive Approach     
+    return self.tree[int(self.rng.random()*len(self.tree))] # OUT OF BOUNDS ERRORS? Check this
     
   def generate_random_node(self, node_to_expand):
     """ Create a new node by expanding from the parent node using.
@@ -105,10 +176,28 @@ class Expansive_Planner():
           node_to_expand (Node): The parent node.
         Returns:
           new_node (Node): The newly generated node.
-    """
-    
+    """    
+    rand_angle = angle_diff(self.rng.random() * 2 * np.pi - np.pi)
+    rand_dist =  self.MIN_RAND_DISTANCE + self.rng.random() * (self.MAX_RAND_DISTANCE- self.MIN_RAND_DISTANCE)
+    # print(rand_dist)
+
+    parent_node_time, parent_node_x, parent_node_y, parent_node_theta = node_to_expand.state
+    random_state = []
+    random_state.append(parent_node_time + rand_dist/self.MEAN_EDGE_VELOCITY)
+    random_state.append(parent_node_x + rand_dist * np.cos(angle_diff(parent_node_theta + rand_angle)))
+    random_state.append(parent_node_y + rand_dist * np.sin(angle_diff(parent_node_theta + rand_angle)))
+    random_state.append(angle_diff(parent_node_theta + 2 * rand_angle))
+
     # Add code here to make a new node #
+    rand_node = Node(random_state, node_to_expand, rand_dist)
     
+    if(not self.collision_found(node_to_expand, rand_node)):
+      # print(f"NO COLLISION {node_to_expand.state} to {rand_node_copy.state}" )
+      # print(rand_node.state, rand_node_copy.state)
+      return rand_node
+
+    # print("COLLISION", rand_node.state)
+
     return None
 
   def generate_goal_node(self, node, desired_state):
@@ -118,9 +207,13 @@ class Expansive_Planner():
         Returns:
           goal_node: The newly generated goal node or None if there is not goal connection.
     """
+    _, traj_distance = construct_dubins_traj(node.state, desired_state)
+    desired_node = Node(desired_state, node, traj_distance)
     
-    # Add code here to make a goal node if possible #
-      
+    if(not self.collision_found(node, desired_node)):
+      # print(f"NO COLLISION FOUND BETWEEN - {node.state} and {desired_node.state}")
+      return desired_node
+    # print(f"COLLISION FOUND BETWEEN - {node.state} and {desired_node.state}")
     return None
 
   def calculate_edge_distance(self, state, parent_node):
@@ -153,15 +246,17 @@ class Expansive_Planner():
   
     traj = []
     traj_cost = 0
+    # print("Building Trajectory")
     for i in range(1,len(node_list)):
       node_A = node_list[i-1]
       node_B = node_list[i]
+      # print(node_A.state)
       traj_point_0 = node_A.state
       traj_point_1 = node_B.state
+      #traj_point_1[3] = math.atan2(traj_point_1[2]-traj_point_0[2], traj_point_1[1]-traj_point_0[1])
       edge_traj, edge_traj_distance = construct_dubins_traj(traj_point_0, traj_point_1)
       traj = traj + edge_traj
       traj_cost = traj_cost + edge_traj_distance
-    
     return traj, traj_cost
 
   def collision_found(self, node_1, node_2):
@@ -174,23 +269,112 @@ class Expansive_Planner():
         Returns:
           collision_found (boolean): True if there is a collision.
     """
-    traj, traj_distance = construct_dubins_traj(node_1.state, node_2.state)
+    traj, _ = construct_dubins_traj(node_1.state, node_2.state)
     return collision_found(traj, self.objects, self.walls)
 
 if __name__ == '__main__':
-  for i in range(0, 5):
-    maxR = 10
-    tp0 = [0, -8, -8, 0]
-    tp1 = [300, 8, 8, 0]
-    planner = Expansive_Planner()
-    walls = [[-maxR, maxR, maxR, maxR, 2*maxR], [maxR, maxR, maxR, -maxR, 2*maxR], [maxR, -maxR, -maxR, -maxR, 2*maxR], [-maxR, -maxR, -maxR, maxR, 2*maxR] ]
-    num_objects = 10
-    objects = []
-    for j in range(0, num_objects): 
-      obj = [random.uniform(-maxR+1, maxR-1), random.uniform(-maxR+1, maxR-1), 1.0]
-      while (abs(obj[0]-tp0[1]) < 1 and abs(obj[1]-tp0[2]) < 1) or (abs(obj[0]-tp1[1]) < 1 and abs(obj[1]-tp1[2]) < 1):
+  
+  plan_budget = 0.5
+  # plan_budgets = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+  # tree_size_limits = [4000, 4000, 4000, 4000, 4000, 100, 500, 1000, 8000, 12000] 
+  # sample_limits = [100, 500, 2000, 4000, 8000, 1000,1000,1000,1000,1000] 
+  tree_size_limits = list(range(100, 600,100))
+  # sample_limits = list(range(500, 5000,500))
+  x = []
+  y = []
+  c = []
+  avg_dist = []
+  N = 200
+  print()
+  print("============= NEW TEST =============")
+  print()
+  for tree_size_limit in tree_size_limits:
+    planner = Expansive_Planner(plan_budget,tree_size_limit)
+    print()
+    print("----------------------------")
+    print(f"Expansive Planner:")
+    print(f"Plan Time Budget: {plan_budget}, Tree Size {tree_size_limit}")
+    tot_dist = 0
+    total_trys = 0 
+    successful_trys = 0
+    failures = 0
+    time_exceeded_count = 0
+    
+
+    for i in range(0, N):
+      # print("Trial #", i + 1)
+      maxR = 10
+      tp0 = [0, -8, -8, 0]
+      tp1 = [300, 8, 8, 0]
+      walls = [[-maxR, maxR, maxR, maxR, 2*maxR], [maxR, maxR, maxR, -maxR, 2*maxR], [maxR, -maxR, -maxR, -maxR, 2*maxR], [-maxR, -maxR, -maxR, maxR, 2*maxR] ]
+      num_objects = 15
+      # objects = [[1,1,3], [3,5,1],[5,7,1],[-2,-7,1], [-5,-7,1]]
+      objects  = []
+      for j in range(0, num_objects): 
         obj = [random.uniform(-maxR+1, maxR-1), random.uniform(-maxR+1, maxR-1), 1.0]
-      objects.append(obj)
-    traj, traj_cost = planner.construct_optimized_traj(tp0, tp1, objects, walls)
-    if len(traj) > 0:
-      plot_traj(traj, traj, objects, walls)
+        while (abs(obj[0]-tp0[1]) < 1 and abs(obj[1]-tp0[2]) < 1) or (abs(obj[0]-tp1[1]) < 1 and abs(obj[1]-tp1[2]) < 1):
+          obj = [random.uniform(-maxR+1, maxR-1), random.uniform(-maxR+1, maxR-1), 1.0]
+        objects.append(obj)
+      traj, traj_cost, total_trials, successful_trials, time_budget_exceeded  = planner.construct_optimized_traj(tp0, tp1, objects, walls)
+    
+      # print("---Running Totals---")
+
+      if(traj_cost < Expansive_Planner.LARGE_NUMBER):
+        tot_dist += traj_cost
+        # print("Distance:        ", tot_dist)
+      else:
+        # print("ATTEMPT FAILED")
+        failures += 1
+
+      if(time_budget_exceeded):
+        time_exceeded_count += 1
+      total_trys+= total_trials
+      successful_trys += successful_trials
+    
+
+
+    
+    # print("Paths Attempted: ", total_trys)
+    # print("Paths Succeeded: ", successful_trys)
+    # print()
+
+    # traj, traj_cost = planner.construct_traj(tp0, tp1, objects, walls)
+    # if len(traj) > 0:
+    #   plot_traj(traj, traj, objects, walls)
+
+    dist_divisor = N - failures
+    # print()
+    # print("Number of trials exceeding budget: ", end="")
+    # print(time_exceeded_count, "/", N, "=", time_exceeded_count/N)
+    
+    if(dist_divisor > 0 and total_trials > 0):
+      c.append(plan_budget)
+      x.append(tree_size_limit)
+      # y.append(tree_size_limit)
+      avg_dist.append((tot_dist/dist_divisor))
+      print("Avg. dist per trial: ", end="")
+      print(tot_dist, "/", dist_divisor, "=", tot_dist/dist_divisor)
+      print("Avg. no of attempts per trial: ", end="")
+      print(total_trys, "/", N, "=", total_trys/N)
+      print("Path Finding Success Rate: ", end="")
+      print(successful_trys, "/", total_trys, "=", successful_trys/total_trys)
+      print()
+
+# fig = plt.figure() 
+  
+# # syntax for 3-D projection 
+# ax = plt.axes(projection ='3d') 
+    
+# plotting 
+# ax.scatter(x, y, avg_dist, c=c) 
+# ax.set_title('Varying Planning Budget') 
+# plt.colorbar()
+# plt.show() 
+plt.plot(x,avg_dist)
+# plt.xlabel('Tree Size')
+# plt.ylabel('Avg Dist')
+plt.show() 
+# plt.scatter(y,avg_dist, c=c)
+# plt.xlabel('Sample Size') 
+# plt.ylabel('Avg Dist')
+# plt.colorbar()
--- Lab4/gym-e206-students-lab-04/traj_planner_utils.py
+++ Lab4/gym-e206-students-lab-04/traj_planner_utils.py
@@ -7,11 +7,9 @@ import math
 import dubins
 import matplotlib.pyplot as plt
 
-DISTANCE_STEP_SIZE = 0.1 #m
-COLLISION_INDEX_STEP_SIZE = 5
+DISTANCE_STEP_SIZE = 0.75 #m
+COLLISION_INDEX_STEP_SIZE = 1
 ROBOT_RADIUS = 0.4 #m
-BUFFER_TIME = 0.4 #s
-COLLISION_BUFFER = 0.2 #m
 
 def construct_dubins_traj(traj_point_0, traj_point_1):
   """ Construc a trajectory in the X-Y space and in the time-X,Y,Theta space.
@@ -22,23 +20,25 @@ def construct_dubins_traj(traj_point_0, traj_point_1):
         traj (list of lists): A list of trajectory points with time, X, Y, Theta (s, m, m, rad).
         traj_distance (float): The length ofthe trajectory (m).
   """
-  q0 = (traj_point_0[1], traj_point_0[2], traj_point_0[3])
-  q1 = (traj_point_1[1], traj_point_1[2], traj_point_1[3])
-  turning_radius = 0.5
-  
-  path = dubins.shortest_path(q0, q1, turning_radius)
-  configurations, distances = path.sample_many(DISTANCE_STEP_SIZE)
-  
-  traj_distance = distances[-1]
-  traj_time = traj_point_1[0] - traj_point_0[0]
-  time_step_size = traj_time/len(distances)
-  
   traj = []
-  traj_point_time = traj_point_0[0]
-  for c in configurations:
-    traj_point = [traj_point_time, c[0], c[1], c[2]]
-    traj.append(traj_point)
-    traj_point_time += time_step_size
+  q0 = traj_point_0[1:]
+  q1 = traj_point_1[1:]
+
+  # Using Linear trajectories
+  # configurations = lpg.construct_linear_path(q0,q1,DISTANCE_STEP_SIZE)
+
+  # Using Dubins trajectories
+  turning_radius = 1
+  path = dubins.shortest_path(q0, q1, turning_radius)
+  configurations, _ = path.sample_many(DISTANCE_STEP_SIZE)
+
+  t_0 = traj_point_0[0]
+  time_step = (traj_point_1[0] - traj_point_0[0])/len(configurations)
+  traj_distance = len(configurations) * DISTANCE_STEP_SIZE
+  
+  for i in range(0, len(configurations)):
+    step = [t_0 + time_step*i, configurations[i][0], configurations[i][1], configurations[i][2]]
+    traj.append(step)
       
   return traj, traj_distance
 
@@ -75,20 +75,17 @@ def plot_traj(traj_desired, traj_actual, objects, walls):
   axis_array[0].plot(x_actual, y_actual, 'k')
 
   ang_res = 0.2
-  for obj in objects:
-    if obj[0] == 'obstacle':
-      o = obj[1]
-      x_obj = []
-      y_obj = []
-      ang = 0
-      while ang < 6.28:
-        x_obj.append(o[0]+o[2]*math.cos(ang))
-        y_obj.append(o[1]+o[2]*math.sin(ang))
-        ang += ang_res
-      x_obj.append(x_obj[0])
-      y_obj.append(y_obj[0])
-      axis_array[0].plot(x_obj, y_obj, 'b')
-    
+  for o in objects:
+    x_obj = []
+    y_obj = []
+    ang = 0
+    while ang < 6.28:
+      x_obj.append(o[0]+o[2]*math.cos(ang))
+      y_obj.append(o[1]+o[2]*math.sin(ang))
+      ang += ang_res
+    x_obj.append(x_obj[0])
+    y_obj.append(y_obj[0])
+    axis_array[0].plot(x_obj, y_obj, 'b')
   for w in walls:
     axis_array[0].plot([w[0], w[2]], [w[1], w[3]], 'k')
   axis_array[0].set_xlabel('X (m)')
@@ -106,64 +103,6 @@ def plot_traj(traj_desired, traj_actual, objects, walls):
 
   plt.show()
   
-def plot_traj_list(traj_list, objects, walls):
-  """ Plot a trajectory in the X-Y space and in the time-X,Y,Theta space.
-      Arguments:
-        desired_traj (list of lists): A list of trajectory points with time, X, Y, Theta (s, m, m, rad).
-        actual_traj (list of lists): A list of trajectory points with time, X, Y, Theta (s, m, m, rad).
-        objects (list of lists): A list of stationay object states with X, Y, radius (m, m, m).
-        walls (list of lists: A list of walls with corners X1, Y1 and X2, Y2 points, length (m, m, m, m, m).
-  """
-  fig, axis_array = plt.subplots(2,1)
-  for traj_desired in traj_list:
-    time_stamp_desired = []
-    x_desired = []
-    y_desired = []
-    theta_desired = []
-    for tp in traj_desired:
-      time_stamp_desired.append(tp[0])
-      x_desired.append(tp[1])
-      y_desired.append(tp[2])
-      theta_desired.append(angle_diff(tp[3]))
-    axis_array[0].plot(x_desired, y_desired, 'b')
-    axis_array[0].plot(x_desired[0], y_desired[0], 'ko')
-    axis_array[0].plot(x_desired[-1], y_desired[-1], 'kx')
-    axis_array[1].plot(time_stamp_desired, x_desired,'b')
-    axis_array[1].plot(time_stamp_desired, y_desired,'b--')
-    axis_array[1].plot(time_stamp_desired, theta_desired,'b-.')
-
-
-  ang_res = 0.2
-  for obj in objects:
-    if obj[0] == 'obstacle':
-      o = obj[1]
-      x_obj = []
-      y_obj = []
-      ang = 0
-      while ang < 6.28:
-        x_obj.append(o[0]+o[2]*math.cos(ang))
-        y_obj.append(o[1]+o[2]*math.sin(ang))
-        ang += ang_res
-      x_obj.append(x_obj[0])
-      y_obj.append(y_obj[0])
-      axis_array[0].plot(x_obj, y_obj, 'b')
-      x_pointers=[o[0],o[0]+o[2]*math.cos(o[4])]
-      y_pointers=[o[1],o[1]+o[2]*math.sin(o[4])]
-      axis_array[0].plot(x_pointers, y_pointers, 'b')
-      
-  for w in walls:
-    axis_array[0].plot([w[0], w[2]], [w[1], w[3]], 'k')
-  axis_array[0].set_xlabel('X (m)')
-  axis_array[0].set_ylabel('Y (m)')
-  axis_array[0].axis('equal')
-  
-  
-
-  axis_array[1].set_xlabel('Time (s)')
-  axis_array[1].legend(['X Desired (m)', 'Y Desired (m)'])
-
-  plt.show()
-
 def collision_found(traj, objects, walls):
   """ Return true if there is a collision with the traj and the workspace
       Arguments:
@@ -176,62 +115,19 @@ def collision_found(traj, objects, walls):
   index = 0
   while index < len(traj):
     traj_point = traj[index]
-    for object in objects:
-      if object[0] == 'obstacle':
-        obj = object[1]
-        obj_radius = obj[2]
-        obj_vel = obj[3]
-        obj_yaw = obj[4]
-        time_stamp = traj_point[0]
-        obj_x = obj[0] + obj_vel*math.cos(obj_yaw)*time_stamp
-        obj_y = obj[1] + obj_vel*math.sin(obj_yaw)*time_stamp
-        obj_distance = generate_distance_to_object(traj_point, [obj_x, obj_y]) - obj_radius - ROBOT_RADIUS - COLLISION_BUFFER
-        if obj_distance < 0:
-          return True
-      elif object[0] == 'traj':
-        if collision_with_traj(object[1], traj_point):
-          return True
-        
+    for obj in objects:
+      obj_distance = generate_distance_to_object(traj_point, obj) - obj[2] - ROBOT_RADIUS
+      if obj_distance < 0:
+        return True
+  
     for wall in walls:
-      wall_distance = generate_distance_to_wall(traj_point, wall) - ROBOT_RADIUS - COLLISION_BUFFER
+      wall_distance = generate_distance_to_wall(traj_point, wall) - ROBOT_RADIUS
       if wall_distance < 0:
         return True
-    
+
     index += COLLISION_INDEX_STEP_SIZE
   
   return False
-  
-def collision_with_traj(traj, traj_point):
-  
-  traj_point_time = traj_point[0]
-  begin_time = traj[0][0]
-  end_time = traj[-1][0]
-  if traj_point_time < begin_time - BUFFER_TIME or traj_point_time > end_time + BUFFER_TIME:
-    return False
-  elif traj_point_time < begin_time:
-    traj_point_at_time = traj[0]
-  elif traj_point_time > end_time:
-    traj_point_at_time = traj[-1]
-  else:
-    time_fraction = traj_point_time / (end_time - begin_time)
-    index = int(time_fraction*len(traj))
-    traj_point_at_time = traj[index]
-  
-  distance = generate_distance_to_traj_point(traj_point, traj_point_at_time) - 2*ROBOT_RADIUS - COLLISION_BUFFER
-  if distance < 0:
-    return True
-  
-  return False
-  
-def generate_distance_to_traj_point(traj_point_0, traj_point_1):
-  """ Calculate the deistance between a spherical object and a cylindrical robot.
-      Argument:
-        traj_point_0 (list of floats): A state of Time, X, Y, Theta (s, m, m, rad).
-        traj_point_1 (list of floats): A state of Time, X, Y, Theta (s, m, m, rad).
-      Returns:
-        distance (float): The distance between a traj point and an object (m).
-  """
-  return math.sqrt( pow(traj_point_0[1]-traj_point_1[1],2) + pow(traj_point_0[2]-traj_point_1[2],2) )
   
 def generate_distance_to_object(traj_point, obj):
   """ Calculate the deistance between a spherical object and a cylindrical robot.
